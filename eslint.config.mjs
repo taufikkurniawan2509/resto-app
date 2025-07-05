255dc0f73f563411692c02315edc8bf1fc43a513
Taufik Kurniawan (Product) <80075887@ioh.co.id>
2025-07-05 20:39:39 +0700

    update unused var

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off", // ⛔ disable rule ini
     },
   },
 ];
